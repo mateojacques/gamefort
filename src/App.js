235b28be81f0af3624ad0468abd3c30c0e4251b9
mateojacques <mateojacquesweb@gmail.com>
2021-12-06 20:18:58 -0300

    Remove excessive API calls from the hompeage

--- src/App.js
+++ src/App.js
@@ -37,93 +37,9 @@ function App() {
                 </Suspense>
                 <Suspense fallback={<p>Loading</p>}>
                 <Featured
-                  title="Browser games"
-                  query="platform=browser"
-                  route="/platform=browser"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
                   title="Recent games"
                   query="sort-by=release-date"
                   route="/sort-by=release-date"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="Shooters"
-                  query="category=shooter"
-                  route="/category=shooter"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="MMORPGs"
-                  query="category=mmorpg"
-                  route="/category=mmorpg"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="Low spec"
-                  query="category=low-spec"
-                  route="/category=low-spec"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="2D games"
-                  query="category=2d"
-                  route="/category=2d"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="3D games"
-                  query="category=3d"
-                  route="/category=3d"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="Fighting games"
-                  query="category=fighting"
-                  route="/category=fighting"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="Card games"
-                  query="category=card"
-                  route="/category=card"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="MOBAs"
-                  query="category=moba&sort-by=popularity"
-                  route="/category=moba&sort-by=popularity"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="Horror games"
-                  query="category=horror"
-                  route="/category=horror"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="Turn-based combat"
-                  query="category=turn-based"
-                  route="/category=turn-based"
-                />
-                </Suspense>
-                <Suspense fallback={<p>Loading</p>}>
-                <Featured
-                  title="Sport games"
-                  query="category=sports"
-                  route="/category=sports"
                 />
                 </Suspense>
               </>
