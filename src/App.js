d116a36c2e7e8c21282588ded4d538f66040b90f
mateojacques <mateojacquesweb@gmail.com>
2021-12-06 20:12:27 -0300

    Added lazy loading effect on main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
-import { useState } from "react";
-import { Sidebar, Hero, Featured, Footer } from "./components";
-import {CategoryGames, GameLauncher} from "./views"
+import { useState, lazy, Suspense } from "react";
+import { Sidebar, Hero, Footer } from "./components";
+import { CategoryGames, GameLauncher } from "./views";
 import { Routes, Route } from "react-router-dom";
+
+const Featured = lazy(() => import("./components/Featured/Featured"));
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -20,66 +22,119 @@ function App() {
 
       <section id="main" className="w-100 d-flex flex-column">
         <Routes>
-          <Route exact path="/" element={<>
-            <Hero />
-            <Featured title="Featured" query="sort-by=relevance" route="/sort-by=relevance" />
-            <Featured
-              title="Browser games"
-              query="platform=browser"
-              route="/platform=browser"
-            />
-            <Featured
-              title="Recent games"
-              query="sort-by=release-date"
-              route="/sort-by=release-date"
-            />
-            <Featured
-              title="Shooters"
-              query="category=shooter"
-              route="/category=shooter"
-            />
-            <Featured title="MMORPGs" query="category=mmorpg" route="/category=mmorpg" />
-            <Featured
-              title="Low spec"
-              query="category=low-spec"
-              route="/category=low-spec"
-            />
-            <Featured title="2D games" query="category=2d" route="/category=2d" />
-            <Featured title="3D games" query="category=3d" route="/category=3d" />
-            <Featured
-              title="Fighting games"
-              query="category=fighting"
-              route="/category=fighting"
-            />
-            <Featured title="Card games" query="category=card" route="/category=card" />
-            <Featured
-              title="MOBAs"
-              query="category=moba&sort-by=popularity"
-              route="/category=moba&sort-by=popularity"
-            />
-            <Featured
-              title="Horror games"
-              query="category=horror"
-              route="/category=horror"
-            />
-            <Featured
-              title="Turn-based combat"
-              query="category=turn-based"
-              route="/category=turn-based"
-            />
-            <Featured
-              title="Sport games"
-              query="category=sports"
-              route="/category=sports"
-            />
-            </>}/>
-            
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <Hero />
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="Featured"
+                  query="sort-by=relevance"
+                  route="/sort-by=relevance"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="Browser games"
+                  query="platform=browser"
+                  route="/platform=browser"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="Recent games"
+                  query="sort-by=release-date"
+                  route="/sort-by=release-date"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="Shooters"
+                  query="category=shooter"
+                  route="/category=shooter"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="MMORPGs"
+                  query="category=mmorpg"
+                  route="/category=mmorpg"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="Low spec"
+                  query="category=low-spec"
+                  route="/category=low-spec"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="2D games"
+                  query="category=2d"
+                  route="/category=2d"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="3D games"
+                  query="category=3d"
+                  route="/category=3d"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="Fighting games"
+                  query="category=fighting"
+                  route="/category=fighting"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="Card games"
+                  query="category=card"
+                  route="/category=card"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="MOBAs"
+                  query="category=moba&sort-by=popularity"
+                  route="/category=moba&sort-by=popularity"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="Horror games"
+                  query="category=horror"
+                  route="/category=horror"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="Turn-based combat"
+                  query="category=turn-based"
+                  route="/category=turn-based"
+                />
+                </Suspense>
+                <Suspense fallback={<p>Loading</p>}>
+                <Featured
+                  title="Sport games"
+                  query="category=sports"
+                  route="/category=sports"
+                />
+                </Suspense>
+              </>
+            }
+          />
 
-          <Route exact path="/:category" element={<CategoryGames/>}/>
+          <Route exact path="/:category" element={<CategoryGames />} />
 
-          <Route exact path="/launcher" element={<GameLauncher/>}/>
+          <Route exact path="/launcher" element={<GameLauncher />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </section>
     </div>
   );
